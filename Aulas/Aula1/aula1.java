41b4eeba61daf9619a8ff04c4d65cae0f4886df3
Pedro370 <72807368+Pedro370@users.noreply.github.com>
2025-02-25 21:06:44 -0300

    aula1

---
+++ Aulas/Aula1/aula1.java
@@ -0,0 +1,85 @@
+class monstro {
+    String name: Dragon
+    int nivel: 70
+
+    void Rugir(){
+        print.String ( "O monstro Rugiu");
+    }
+}
+
+
+class Jogador {
+    String name:Raple
+    int pontuacao;
+    int nivel: 187
+
+    
+    void SubirdeNivel() {
+    print.String ("O jogador subiu de nivel");
+    }
+}
+
+class Inimigo {
+    String name: Radanh
+    String type: Human
+    int forca: 200
+
+    void atacar() {
+        print.String ("O inimigo atacou com força,"X);
+    }
+}
+
+class Arma {
+    String Name:Moonligth
+    int dano: 573
+
+    void usar() {
+        print.String("A arma foi usada para atacar causando"x "de dano");
+    }
+}
+
+class Inventário {
+
+    void adcionarItem() {
+        print.String()
+    }
+}
+
+class perssonagem {
+    String: Ranni
+    int vida: 1200
+    int nivel:197
+
+    void MostraStatus() {
+        print.String()
+    }
+}
+
+class Lampada {
+
+    void ligar() {
+        print.String("A Lâmpada foi Acesa!");
+
+    
+    void Desligar(){
+        print.String("A Lâmpada foi Desligada");
+        }
+    }
+}
+
+class Usuário {
+    String name:usuario
+    int senha:1234
+}
+
+class Heroi {
+    String name: Sigfrield
+    String classe: EspadachimMagíco
+    int dano: 573
+
+
+    void atacar() {
+        print.String("O Heroi Atacou causando"x "de Dano");
+    }
+
+}
